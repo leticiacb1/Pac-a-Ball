dbe87194f3e3f29ca5a70e8c611239ada1762cb1
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-09-17 21:05:11 -0300

    Change scene manager name.

--- Assets/Scripts/digitalClock.cs
+++ Assets/Scripts/digitalClock.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 public class digitalClock : MonoBehaviour
 {
     public float TimeLeft = 120.0f;
     public bool TimerOn = false;
+    private int END_GAME_SCENE = 2;
 
     public TextMeshProUGUI TimerTxt;
    
@@ -28,6 +30,8 @@ public class digitalClock : MonoBehaviour
                 Debug.Log("Go to Game Over Screen");
                 TimeLeft = 0;
                 TimerOn = false;
+
+                SceneManager.LoadScene(END_GAME_SCENE);
             }
         }
     }
